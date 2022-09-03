48e7591da2bd8df5cb51dad995f22c826c5f8a5b
Martin Zikmund <martin@zikmund.dev>
2022-09-03 11:23:38 +0200

    Start icon font updater

---
+++ src/CommandOptions.cs
@@ -0,0 +1,15 @@
+﻿using CommandLine;
+
+namespace Uno.IconFontUpdater;
+
+public class CommandOptions
+{
+	[Option('r', "repository", Required = true, HelpText = "Root folder of the cloned repository.")]
+	public string RepositoryPath { get; set; }
+
+	[Option('t', "ttf-font", Required = true, HelpText = "Path to the new icon font .ttf.")]
+	public string TtfPath { get; set; }
+
+    [Option('w', "woff2-font", Required = true, HelpText = "Path to the new icon font .woff2.")]
+    public string Woff2Path { get; set; }
+}
---
+++ src/Program.cs
@@ -0,0 +1,21 @@
+﻿using CommandLine;
+using Uno.IconFontUpdater;
+
+const string IconFontName = "uno-fluentui-assets.ttf";
+
+var parseResult = Parser.Default.ParseArguments<CommandOptions>(args);
+if (parseResult.Errors.Any() || parseResult.Value is null)
+{
+    Console.WriteLine("Invalid arguments");
+    return;
+}
+
+CommandOptions options = parseResult.Value;
+var repositoryPath = options.RepositoryPath;
+
+
+
+void ReplaceIconFontFile(string iconFontFilePath)
+{
+    File.Replace(options.TtfPath, iconFontFilePath, null);
+}
