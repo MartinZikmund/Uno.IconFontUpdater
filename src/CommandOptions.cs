22a303d61b31bbdc0e4fc4fe5f67e03fc145d5ea
Martin Zikmund <martin@zikmund.dev>
2022-09-04 10:23:17 +0200

    File and CSS font updater

--- src/CommandOptions.cs
+++ src/CommandOptions.cs
@@ -11,5 +11,5 @@ public class CommandOptions
 	public string TtfPath { get; set; }
 
     [Option('w', "woff2-font", Required = true, HelpText = "Path to the new icon font .woff2.")]
-    public string Woff2Path { get; set; }
+    public string WoffPath { get; set; }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,11 @@
 ﻿using CommandLine;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 using Uno.IconFontUpdater;
 
 const string IconFontName = "uno-fluentui-assets.ttf";
+const string CssFontRegex = @"(font-family: ""Symbols"";[^\}]*src:)(.*format\('woff'\);)";
+const string CssFontFormatString = "url(data:application/x-font-woff;charset=utf-8;base64,{0}) format('woff');";
 
 var parseResult = Parser.Default.ParseArguments<CommandOptions>(args);
 if (parseResult.Errors.Any() || parseResult.Value is null)
@@ -12,10 +16,49 @@ if (parseResult.Errors.Any() || parseResult.Value is null)
 
 CommandOptions options = parseResult.Value;
 var repositoryPath = options.RepositoryPath;
+var cssFontDeclaration = CreateWoffCssDeclaration();
+UpdateDirectory(new DirectoryInfo(repositoryPath));
 
+void UpdateDirectory(DirectoryInfo directory)
+{
+    foreach (var file in directory.EnumerateFiles("*.ttf"))
+    {
+        if (file.Name == IconFontName)
+        {
+            Console.WriteLine($"Updating {file.FullName}");
+            ReplaceIconFontFile(file.FullName);
+        }
+    }
 
+    foreach (var file in directory.EnumerateFiles("*.css"))
+    {
+        ReplaceCssFont(file.FullName);
+    }
 
+    foreach (var subDirectory in directory.EnumerateDirectories())
+    {
+        UpdateDirectory(subDirectory);
+    }
+}
+                
 void ReplaceIconFontFile(string iconFontFilePath)
 {
-    File.Replace(options.TtfPath, iconFontFilePath, null);
+    File.Copy(options.TtfPath, iconFontFilePath, true);
+}
+
+string CreateWoffCssDeclaration()
+{
+    var bytes = File.ReadAllBytes(options.WoffPath);
+    var base64FontData =System.Convert.ToBase64String(bytes);
+    return string.Format(CssFontFormatString, base64FontData);
+}
+
+void ReplaceCssFont(string cssFilePath)
+{
+    var fileContent = File.ReadAllText(cssFilePath);
+    if (Regex.IsMatch(fileContent, CssFontRegex))
+    {
+        fileContent = Regex.Replace(fileContent, CssFontRegex, m => m.Groups[1].Value + cssFontDeclaration);
+        File.WriteAllText(cssFilePath, fileContent);
+    }
 }
